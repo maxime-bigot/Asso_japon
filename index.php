c219599ec6389424dd645a3557fd8ebd3d4cb129
maxime <maxime.bigot@viacesi.fr>
2016-10-28 14:20:56 +0200

    système de pagination
    pagination sur l'accueil pour voir les anime sur plusieurs pages.

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 session_start();
 $bdd = new PDO("mysql:host=127.0.0.1;dbname=anime;charset=utf8", "root", "");
 
-$animes = $bdd->query('SELECT * FROM anime ORDER BY Titre_anime');
+$videosParPage = 5;
+$videoTotal = $bdd->query('SELECT ID_anime FROM anime');
+$videoTotal = $videoTotal->rowCount();
+$pagesTotales = ceil($videoTotal/$videosParPage);
+
+if(isset($_GET['page']) AND !empty($_GET['page']) AND $_GET['page'] > 0) {
+	$page = intval($_GET['page']);
+} else {
+	$page = 1;
+}
+
+$depart = ($page-1)*$videosParPage;
+
+$animes = $bdd->query('SELECT * FROM anime ORDER BY Titre_anime LIMIT '.$depart.','.$videosParPage);
 
 if(isset($_GET['q']) AND !empty($_GET['q'])){
 	$q = htmlspecialchars($_GET['q']);
 
-	$animes = $bdd->query('SELECT * FROM anime WHERE Titre_anime LIKE "%'.$q.'%" ORDER BY Titre_anime');
+	$animes = $bdd->query('SELECT * FROM anime WHERE Titre_anime LIKE "%'.$q.'%" ORDER BY Titre_anime LIMIT '.$depart.','.$videosParPage);
 }
 
 $categories = $bdd->query('SELECT COUNT(anime_genre.ID_genre), genre.Nom_genre, genre.id_genre FROM anime_genre INNER JOIN genre ON anime_genre.ID_genre = genre.id_genre GROUP BY genre.Nom_genre, genre.id_genre');
@@ -50,7 +63,21 @@ $categories = $bdd->query('SELECT COUNT(anime_genre.ID_genre), genre.Nom_genre, 
 			<li><a href="anime.php?id=<?= $a['ID_anime']?>"><?= $a['Titre_anime'] ?></a></li>
 		</div>
 		<?php } ?>
-	</ul></br></br>
+	</ul></br></br><p>Page : </p>
+	<?php
+	for ($i=1; $i <= $pagesTotales; $i++) { 
+		if($i != $page) {
+			if(isset($q)) {
+				echo "<a href=\"index.php?q=".$q."&page=".$i."\">".$i."</a> ";
+			} else {
+				echo "<a href=\"index.php?page=".$i."\">".$i."</a> ";
+			}
+		} else {
+			echo $i." ";
+		}
+		
+	}
+	?></br></br>
 	<a href="redaction.php">Ajouter un anime</a></br></br>
 
 	<h2>Catégories</h2></br></br>
